3ea2a0c306ba2937eb3adba97ff20214c3c47610
Charles Vandal <charlesvandal@gmail.com>
2026-09-20 15:59:47 -0400

    Ran linter

--- Src/Object/Fire.cpp
+++ Src/Object/Fire.cpp
@@ -1,13 +1,13 @@
 #include "Fire.hpp"
 
-Fire::Fire(const std::string& name) : Object(ObjectType::FIRE, name) {}
+Fire::Fire(const std::string &name) : Object(ObjectType::FIRE, name) {}
 
 bool Fire::isFren(void)
 {
     return false;
 }
 
-void Fire::print(std::ostream& os) const
+void Fire::print(std::ostream &os) const
 {
     os << "X";
 }
